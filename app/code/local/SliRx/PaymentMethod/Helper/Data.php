8912b94b70f3d30d8d6a484123248f9cd72fa6c0
slirx <karazey.sergey@gmail.com>
2014-06-22 22:35:12 +0300

    Add the base of payment method

---
+++ app/code/local/SliRx/PaymentMethod/Helper/Data.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @author     Karazey Sergey <karazey.sergey@gmail.com>
+ * @copyright  2014 Karazey Sergey
+ * @created    6/22/14 7:10 PM
+ */
+class SliRx_PaymentMethod_Helper_Data extends Mage_Core_Helper_Abstract
+{
+
+}
---
+++ app/code/local/SliRx/PaymentMethod/Model/TestPaymentMethod.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @author     Karazey Sergey <karazey.sergey@gmail.com>
+ * @copyright  2014 Karazey Sergey
+ * @created    6/22/14 7:13 PM
+ */
+class SliRx_PaymentMethod_Model_TestPaymentMethod extends Mage_Payment_Model_Method_Abstract
+{
+    protected $_code = 'test_payment_method';
+}
